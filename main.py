7a3d0b94dc7a6af7f11e5167db043d354ddd11df
Gus Park <parkgusk@gmail.com>
2020-07-13 16:44:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num < 10:
+  print("You number is less than 10")
+if num > 10:
+  print("Your number is greater than 10")
+if num == 10:
+  print("Your number is 10")
